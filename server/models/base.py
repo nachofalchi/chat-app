b8e074312e224ed73978312032704513febc0a0d
nachofalchi <nachofalchi@hotmail.com>
2024-12-08 23:30:02 -0300

    New database conection

--- server/models/base.py
+++ server/models/base.py
@@ -2,7 +2,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 Base = declarative_base()
-engine = create_engine('sqlite:///app.db')
+engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
